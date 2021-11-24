c0f82a800d1481a21135cb4aaeddf2d9dd9303c1
Nicholas Nolen <nicnolen@ymail.com>
2021-11-24 15:27:49 -0600

    made a timer appear when start button is clicked

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,19 @@ function startGame() {
     startBox.classList.add('hide');
     // unhide quiz box
     quizBox.classList.remove('hide')
-}
+
+    // create a countdown from 60 seconds (60000 milliseconds)
+    var timeLeft = 60 // counter starts counting down from 60 seconds
+    var countdown = setInterval(function(){
+        if(timeLeft <= 0) {
+            clearInterval(countdown); // clearInterval stops the countdown function from running
+            document.getElementById('timer').textContent = "Time's up!"
+        } else {
+            document.getElementById('timer').textContent = timeLeft + " seconds remaining";
+        }
+        timeLeft--; // decrease the timer by 1 every second (1000ms = 1s)
+    }, 1000);
+};
 // EVENT LISTENERS
 // Run startGame when the start button is clicked
 startButton.addEventListener('click', startGame);
